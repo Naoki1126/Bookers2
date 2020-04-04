71eb5d9a7e62641ae3332302f6cb41c8eed25686
丸山直城 <maruyamanaoki@maruyamatadashijounoMacBook-Air.local>
2020-04-04 23:26:40 +0900

    [Finish]ouyou-4

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,41 @@
+class RelationshipsController < ApplicationController
+
+
+	def follows
+		@user = User.find(params[:id])
+		@users = @user.followings
+	end
+
+	def followers
+		@user = User.find(params[:id])
+		@users = @user.followers
+	end
+
+	def create
+		user = User.find(params[:follow_id])
+		following = current_user.follow(user)
+		if following.save
+			flash[:success] = 'フォロー中'
+			redirect_back(fallback_location: root_path)
+
+		else
+			flash.now[:alert] = 'ユーザーのフォローに失敗しました'
+			redirect_back(fallback_location: root_path)
+
+		end
+	end
+
+	def destroy
+		user = User.find(params[:follow_id])
+		following = current_user.unfollow(user)
+		if following.destroy
+			flash[:success] = 'フォロー'
+			redirect_back(fallback_location: root_path)
+
+		else
+			flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
+			redirect_back(fallback_location: root_path)
+
+		end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,16 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:show, :edit, :index, :update]
+  resources :relationships, only: [:create, :destroy]
   resources :homes, only: [:home]
   
+  get 'users/:id/follows' => 'relationships#follows'
+  get 'users/:id/followers' => 'relationships#followers'
   get 'home/about' => 'homes#about'
   root 'homes#home'
 
+  
 end
+
 
   # For details onra the DSL available within this file, see http://guides.rubyonrails.org/routing.html
