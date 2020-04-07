70d2a9dcab87bd03f420a404e446ddaee0d1c3dd
丸山直城 <maruyamanaoki@maruyamatadashijounoMacBook-Air.local>
2020-04-07 21:54:30 +0900

    [Finish]ouyou6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,22 +1,22 @@
 class BookCommentsController < ApplicationController
 	def create
-		book = Book.find(params[:book_id])
-		comment = current_user.book_comments.new(book_comment_params)
-		comment.book_id = book.id
-		comment.save
-		redirect_to book_path(book)
+		@book = Book.find(params[:book_id])
+		@comment = current_user.book_comments.new(book_comment_params)
+		@comment.book_id = @book.id
+		@comment.save
+		
 
 
 	end
 
 	def destroy
-		book = Book.find(params[:book_id])
-		book_comment = BookComment.find_by(
+		@book = Book.find(params[:book_id])
+		@book_comment = BookComment.find_by(
 			user_id: current_user.id,
 			book_id: params[:book_id])
 		
-		book_comment.destroy
-		redirect_to book_path(book)
+		@book_comment.destroy
+		
 	end
 private
 def book_comment_params
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,16 @@
 class FavoritesController < ApplicationController
 
 	def create
-		book = Book.find(params[:book_id])
-		favorite = current_user.favorites.new(book_id: book.id)
+		@book = Book.find(params[:book_id])
+		favorite = current_user.favorites.new(book_id: @book.id)
 		favorite.save
-		redirect_back(fallback_location: root_path)
 	end
 	def destroy
-		book = Book.find(params[:book_id])
+		@book = Book.find(params[:book_id])
 		favorite =  Favorite.find_by(
 			user_id: current_user.id,
 			book_id: params[:book_id])
 		favorite.destroy
-		redirect_back(fallback_location: root_path)
 	end
 
-private
-	def favorite_params
-		params.require(:favorite).permit(:user_id, :book_id)
-	end
 end
